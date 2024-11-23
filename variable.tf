9c099593ba3debebf9736a5b878390539f887ddc
Shubham6591 <shubhamgupta6591@gmail.com>
2024-11-23 11:32:46 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,10 @@
+variable "bucket_name" {
+ description = "Name of the S3 bucket"
+ type = string
+
+}
+variable "acl" {
+ description = "Access control list for the bucket (e.g., private, public-read)"
+ type = string
+ default = "private"
+} 
